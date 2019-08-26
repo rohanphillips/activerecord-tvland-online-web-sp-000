8df6cb396d33d049c84b2288081c6c36a044ec21
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-26 21:40:58 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :shows, through: :characters
+    has_many :characters
+
+    def full_name
+      self.first_name + " " + self.last_name
+    end
+
+    def list_roles
+      collection = []
+      self.characters.joins(:show).each{|item|
+        collection << (item.name + " - " + item.show.name)
+      }
+      collection
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :actors, through: :characters
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    collection = []
+    self.characters.joins(:actor).each{|item|
+      collection << (item.actor.first_name + " " + item.actor.last_name)
+    }
+    collection
+  end
+
 end
