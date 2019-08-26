4af50217fe2b6e9d92502187cec92415b2ee6ff5
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-26 20:45:34 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
     def list_roles
 
-      records = Character.join(:shows, :actors)
+      records = Character.joins(:shows, :actors)
       binding.pry
       self.characters.name
     end
