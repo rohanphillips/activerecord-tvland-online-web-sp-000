01ddc59166d8b74696cb6e7babe46873ace73dce
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-26 20:23:20 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,8 @@
 class Actor < ActiveRecord::Base
     has_many :shows, through: :characters
     has_many :characters
-    def full_name
 
+    def full_name
+      self.first_name + " " self.last_name
     end
 end
